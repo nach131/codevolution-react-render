e4811a3579e81ff4ef651f1d0cd23f69b1805f67
nacho <nacho.mota@gmail.com>
2021-08-07 23:01:47 +0200

    Inicio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <button type="button" className="btn btn-outline-warning">Warning</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
