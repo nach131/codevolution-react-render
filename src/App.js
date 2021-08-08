cc0b22dccae8f5d9168c173a4ccd234826cd8879
nacho <nacho.mota@gmail.com>
2021-08-08 11:48:09 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button } from 'react-bootstrap';
+import { ObjectUseState } from './component/Immutable Strate/ObjectUseState';
+import { ArrayUseState } from './component/Immutable Strate/ArrayUseState';
+// import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <Button variant="outline-info">Info</Button>
-
+        <div className="row">
+          <div className="col">
+            <h5>ObjectUseState</h5>
+            <ObjectUseState />
+          </div>
+          <div className="col">
+            <h5>ArrayUseState</h5>
+            <ArrayUseState />
+          </div>
+        </div>
       </header>
     </div>
   );
